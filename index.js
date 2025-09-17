8e261f04f3f34234d43472c112f9080fb9b42c93
Abishek krishnamoorthy <102430868+abishekkrishnamoorthy@users.noreply.github.com>
2025-09-17 15:38:19 +0530

    cors commit

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const useroute = require("./routes/userRoutes");
 const cors = require("cors");
 require("dotenv").config();
 const app=express()
-app.use(cors());
-app.use(cors());
-   
+app.use(cors());   
 
 connectDB(); 
 app.use(express.json());
